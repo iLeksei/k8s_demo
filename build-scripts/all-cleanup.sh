729b0fc9d1051bab25ffc3e55646b849ed2c7227
ileksei <dev.zhuravlev@gmail.com>
2023-12-17 01:41:32 +0300

    added cleanup script

---
+++ build-scripts/all-cleanup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#remove services
+echo "Removing services..."
+kubectl delete -f ./kubernetes/2_services/config_server_service.yaml
+kubectl delete -f ./kubernetes/2_services/examinator_service.yaml
+kubectl delete -f ./kubernetes/2_services/question_services.yaml
+
+#remove pods
+echo "Removing pods..."
+kubectl delete -f ./kubernetes/3_pods/pods.yaml
+
+#remove docker images
+echo "Removing images..."
+
+docker rmi application/provider-js-questions
+docker rmi application/provider-java-questions
+docker rmi application/examinator
+docker rmi application/eureka-discovery-service
+docker rmi application/config-server
+
+exec $SHELL;
+#/bin/bash
